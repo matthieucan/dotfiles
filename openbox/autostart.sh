b9c07101005d8b5643a50c269889d9ac78ef2946
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-07-10 23:35:12 +0200

    add: openbox configuration files

---
+++ openbox/autostart.sh
@@ -0,0 +1,13 @@
+tint2 &
+xscreensaver -no-splash &
+thunar --daemon &
+#thunar-volman &
+volti &
+conky &
+osdsh &
+(sleep 1 && osdctl -m 1) &
+#gnome-volume-manager &
+gnome-sound-applet &
+gnome-settings-daemon &
+
+#(sleep 3 && nitrogen --restore) &
