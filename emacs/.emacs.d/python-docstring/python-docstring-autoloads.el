69fe9e7f874bfad207f23ebbe46c3079210a92eb
Matthieu Caneill <matt@brokenwa.re>
2018-05-17 01:08:01 +0200

    remove python-docstring, install from melpa

--- emacs/.emacs.d/python-docstring/python-docstring-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; python-docstring-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("python-docstring-pkg.el" "python-docstring.el")
-;;;;;;  (22093 53982 725834 517000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; python-docstring-autoloads.el ends here
--- emacs/.emacs.d/python-docstring/python-docstring-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-docstring" "20150907.1304" "Smart Python docstring formatting" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
