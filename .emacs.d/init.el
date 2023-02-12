7d3131fc68f12d806f893b1bc70d7f55d694b29b
Matthieu Caneill <matt@brokenwa.re>
2023-02-12 23:37:19 +0100

    emacs: projectile/counsel-projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -754,11 +754,27 @@
 
 ;; https://github.com/bbatsov/projectile
 ;; project-level commands
-;; (use-package projectile
-;;   :config
-;;   (projectile-mode 1)
-;;   (setq projectile-completion-system 'ivy)
-;;   )
+
+(use-package projectile
+  :init
+  (projectile-mode +1)
+
+  :bind
+  (:map projectile-mode-map
+        ("s-p" . projectile-command-map)
+        ("C-c p" . projectile-command-map))
+
+  :config
+  (projectile-mode 1)
+  (setq projectile-completion-system 'ivy)
+  )
+
+(use-package counsel-projectile
+  :after projectile
+
+  :config
+  (counsel-projectile-mode)
+  )
 
 ;; ====================
 ;; deadgrep
