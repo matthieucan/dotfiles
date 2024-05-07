ea5b1275b979b9dd78ab9ccebe684bdd6feff014
Matthieu Caneill <matthieu.caneill@teampicnic.com>
2024-05-07 11:13:47 +0200

    org: rename capture template

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -412,7 +412,7 @@
            "* %?\n  Entered on %U\n%i\n  %a")
           ("J" "Journal (displayed in agenda)" entry (file+datetree "~/org/log.org")
            "* %?\n  Entered on %T\n%i\n  %a")
-          ("p" "Note (work)" entry (file+olp+datetree "~/org/picnic.org" "Notes" "Captured")
+          ("p" "Picnic note" entry (file+olp+datetree "~/org/picnic.org" "Notes" "Captured")
            "* Note: %?                                                          :note:\n%T\n"
            :empty-lines 1 :empty-lines-after 2 :clock-keep t :tree-type month)
           ))
